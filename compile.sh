bb2cb4e288efeec4f65aa724e53c78adbaf15f4b
H. Gabriel Maculus <gabrielmaculus@gmail.com>
2026-03-15 17:19:31 -0300

    compile.sh add std=c17

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
-clang -fPIC -c file.c -o file.o
-clang -fPIC -c string.c -o string.o
-clang -shared -o libstdgm.so file.o string.o
+clang -Wall -std=c17 -c file.c -o file.o
+clang -Wall -std=c17 -c string.c -o string.o
+#clang -Wall -std=c17 -fPIC -fPIE -c main.c -o main.o
+clang -Wall -std=c17 -shared -o libstdgm.so file.o string.o
